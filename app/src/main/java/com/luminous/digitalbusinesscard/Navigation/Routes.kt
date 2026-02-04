b28af29839ff4e5149995edacdfabde7de07215d
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2026-02-04 20:21:36 +0600

    Routes

--- app/src/main/java/com/luminous/digitalbusinesscard/Navigation/Routes.kt
+++ app/src/main/java/com/luminous/digitalbusinesscard/Navigation/Routes.kt
@@ -1,5 +1,6 @@
 package com.luminous.digitalbusinesscard.Navigation
 
+// Declares a Kotlin "object" (a singleton class compared to Swift).
 object Routes {
         const val Profile = "profile"
         const val Settings = "settings"
