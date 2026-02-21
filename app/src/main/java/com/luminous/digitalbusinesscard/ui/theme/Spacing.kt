85e1ab4e558f78a47cefe7b800e66cb8948ce847
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2026-02-21 23:57:01 +0600

    spacing

--- app/src/main/java/com/luminous/digitalbusinesscard/ui/theme/Spacing.kt
+++ app/src/main/java/com/luminous/digitalbusinesscard/ui/theme/Spacing.kt
@@ -1,2 +1,11 @@
 package com.luminous.digitalbusinesscard.ui.theme
 
+// dp = “density-independent pixels”
+import androidx.compose.ui.unit.dp
+data class Spacing(
+        val xSmall: androidx.compose.ui.unit.Dp = 4.dp,
+        val small: androidx.compose.ui.unit.Dp = 8.dp,
+        val medium: androidx.compose.ui.unit.Dp = 16.dp, // default padding
+        val large: androidx.compose.ui.unit.Dp = 24.dp,
+        val xLarge: androidx.compose.ui.unit.Dp = 32.dp
+)
