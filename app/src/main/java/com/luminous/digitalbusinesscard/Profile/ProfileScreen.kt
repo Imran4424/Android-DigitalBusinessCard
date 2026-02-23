4658f7858bce36076f02555701ed6f72c9b25585
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2026-02-24 01:01:34 +0600

    profile screen

---
+++ app/src/main/java/com/luminous/digitalbusinesscard/Profile/ProfileScreen.kt
@@ -0,0 +1,13 @@
+package com.luminous.digitalbusinesscard.Profile
+
+import androidx.compose.runtime.Composable
+import com.luminous.digitalbusinesscard.ui.theme.LocalSpacing
+
+@Composable
+fun ProfileScreen(
+        onGoToSettings: () -> Unit
+) {
+        val localSpacing = LocalSpacing.current
+        
+
+}
